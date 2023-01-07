ef4570d403eb6fc74d10fc4f7d809ba839d6e0ec
Muhindo-Galien <muhindogalien512@gmail.com>
2023-01-07 04:31:51 +0300

    SwapTkoen initialized

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,12 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import "@typechain/hardhat"
+import "@nomiclabs/hardhat-waffle"
+import "@nomiclabs/hardhat-etherscan"
+import "@nomiclabs/hardhat-ethers"
+import "hardhat-gas-reporter"
+import "dotenv/config"
+import "solidity-coverage"
+import "hardhat-deploy"
+import { HardhatUserConfig } from "hardhat/config"
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
